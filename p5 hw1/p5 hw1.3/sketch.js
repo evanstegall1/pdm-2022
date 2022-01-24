40b911f1ec94f0d6cbc3be5a5638ee0893307094
evanstegall1 <evanstegall@icloud.com>
2022-01-23 22:01:50 -0600

    formatted code

--- p5 hw1/p5 hw1.3/sketch.js
+++ p5 hw1/p5 hw1.3/sketch.js
@@ -4,15 +4,19 @@ function setup() {
 
 function draw() {
   background(0);
+
   //pac-man figures
   noStroke();
+
   //pac-man body
   fill(255, 255, 0);
   arc(100, 100, 160, 160, PI + QUARTER_PI, PI - QUARTER_PI);
+
   //ghost body
   fill(255, 50, 0);
   rect(220, 100, 160, 80);
   arc(300, 100, 160, 160, PI, 0);
+  
   //ghost eyes
   fill(255);
   circle(260, 100, 50);

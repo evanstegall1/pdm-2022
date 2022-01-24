627d8ee2af7a06607673f217e13c07c86e05480d
evanstegall1 <evanstegall@icloud.com>
2022-01-23 22:00:09 -0600

    formatted code

--- p5 hw1/p5 hw1.2/sketch.js
+++ p5 hw1/p5 hw1.2/sketch.js
@@ -4,14 +4,18 @@ function setup() {
 
 function draw() {
   background(255);
+
 //circles
   noStroke();
+
 //red
   fill(255, 0, 0, 80);
   circle(200, 133, 220);
+
 //blue
   fill(0, 0, 255, 80);
   circle(133, 267, 220);
+  
 //green
   fill(0, 255, 0, 80);
   circle(267, 267, 220);

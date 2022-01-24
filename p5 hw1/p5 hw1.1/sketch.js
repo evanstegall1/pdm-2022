f05876e734853fbacf33d6e0f95ee91a2585c6bf
evanstegall1 <evanstegall@icloud.com>
2022-01-23 19:49:17 -0600

    resized 1.1 and added template for 1.2

---
+++ p5 hw1/p5 hw1.1/sketch.js
@@ -0,0 +1,9 @@
+function setup() {
+  createCanvas(400, 200);
+}
+
+function draw() {
+  background(0, 255, 0);
+  circle(100, 100, 160);
+  square(220, 20, 160);
+}
--- p5 hw1/sketch.js
+++
@@ -1,9 +0,0 @@
-function setup() {
-  createCanvas(800, 400);
-}
-
-function draw() {
-  background(0, 255, 0);
-  circle(200, 200, 320);
-  square(440, 40, 320);
-}

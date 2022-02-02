e0c248d530ed30a8ac83f82b579b57fba12fcf66
evanstegall1 <evanstegall@icloud.com>
2022-02-01 23:46:57 -0600

    finished hw 2

--- painting application/sketch.js
+++ painting application/sketch.js
@@ -1,12 +1,45 @@
+let a = 40;
+let b = 4;
+
+let c1 = 'black';
+let c2 = 'red';
+/* let c3 = 'green';
+let c4 = 'blue';
+let c5 = 'yellow';
+let c6 = 'orange';
+let c7 = 'pink';
+let c8 = 'white'; */
+
+let x = a / 2;
+let y = a / 2;
+
+let d1 = dist(mouseX, mouseY, a / 2, a / 2);
+let d2 = dist(mouseX, mouseY, a / 2, 3 * a / 2);
+
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(800, 400);
 }
 
 function draw() {
-  let c = 'black';
-  
-  while (mouseIsPressed) {
-    fill(c);
-    ellipse(mouseX, mouseY, 4, 4);
+  fill(c1);
+  circle(a / 2, a / 2, a);
+  fill(c2);
+  circle(a / 2, 3 * a / 2, a);
+
+  if (d1 < a / 2) {
+    mouseReleased()
+  }
+  if (d2 < a / 2) {
+    mouseReleased()
+  }
+  if (mouseIsPressed) {
+    mouseReleased()
+  } 
+}
+
+function mouseReleased() {
+  if (d1 < a) {
+    
   }
 }
+

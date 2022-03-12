aad0cef1f17fb390ef637d9cb57e21cf0bef5c55
evanstegall1 <evanstegall@icloud.com>
2022-03-11 19:28:29 -0600

    finished sampler

--- sampler/sketch.js
+++ sampler/sketch.js
@@ -1,15 +1,10 @@
-//Using buttons and sliders
-
-//variables used here
 let sounds, delay, delSlide, dy, y0;
 dy = 10;
-y0 = 100;
+y0 = 120;
 
 function preload(){
-  //makes an audio effect. We connect it to the destination speakers
   delay = new Tone.FeedbackDelay(0.25, 0.5).toDestination();
-  
-  //holds our soundfiles for playback. Is connected to the delay effect
+
   sounds = new Tone.Players({
     kick: "media/sweetkick.mp3",
     snare: "media/wonderfulsnare.mp3",
@@ -21,28 +16,27 @@ function preload(){
 
 function setup() {
   createCanvas(400, 400);
-  //makes the buttons. we need to say what is displayed on the button, the position, and what happens when clicked
-  kickButton = createButton("Kick");
+  
+  kickButton = createButton("Kick (1)");
   kickButton.position((width - kickButton.width) / 2, y0);
   kickButton.mousePressed(kickPB);
   
-  snareButton = createButton("Snare");
+  snareButton = createButton("Snare (2)");
   snareButton.position((width - snareButton.width) / 2, y0 + snareButton.height + dy);
   snareButton.mousePressed(snarePB);
 
-  hatButton = createButton("Hat");
+  hatButton = createButton("Hat (3)");
   hatButton.position((width - hatButton.width) / 2, y0 + 2 * (hatButton.height + dy));
   hatButton.mousePressed(hatPB);
   
-  crashButton = createButton("Crash");
+  crashButton = createButton("Crash (4)");
   crashButton.position((width - crashButton.width) / 2, y0 + 3 * (crashButton.height + dy));
   crashButton.mousePressed(crashPB);
 
-  tomButton = createButton("Tom");
+  tomButton = createButton("Tom (5)");
   tomButton.position((width - tomButton.width) / 2, y0 + 4 * (tomButton.height + dy));
   tomButton.mousePressed(tomPB);
 
-  //makes the slider. need to set the values of the slider, and the position
   delSlide = createSlider(0, 1, 1, 0);
   delSlide.position(width / 3, 300);
 }
@@ -50,21 +44,23 @@ function setup() {
 function draw() {
   background(220);
 
-  //assigns the value of the slider to the desired control. must happen in the draw loop to live update.
+  textSize(35);
+  textAlign(CENTER);
+  text('Drum Sampler', width / 2, 50);
+  textSize(14);
+  text('Click the buttons or use the Numbered Keys to play:', width / 2, 90);
+
   delay.wet.value = delSlide.value();
-  // label for the slider. doesn't happen by default. Don't forget to label your sliders
+
   textAlign(CENTER);
   textStyle(BOLDITALIC);
   textSize(14)
   text("Delay (wet/dry)", width / 2, 300);
   
-  //visualization of the current position of the slider
   let wetAmount = floor(delSlide.value() * 100);
   text(`${wetAmount}%`, width / 2, 330);
 }
 
-
-//call back functions to be called when a button is pressed. One for each button. These just playback a sound.
 function kickPB(){
   sounds.player("kick").start();
 }
